41f3de111a457113b10e1a60ede8dbc5f290cbdd
DavidColesDC <me@davidcoles.uk>
2024-09-03 20:23:04 +0100

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-gray-200">
-      <div className="font-extralight text-9xl">David Coles</div>
+    <main>
+        <div className={'david-container'}>
+            <Image alt={'David Coles'} height={100} src={'/images/david-coles.jpg'} width={100}></Image>
+        </div>
+        <div className="font-extralight text-9xl">David Coles</div>
     </main>
   );
 }
